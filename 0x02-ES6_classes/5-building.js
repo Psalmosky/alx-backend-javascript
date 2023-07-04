1261558bbd60e99c22e45f4bc9c9c81680c94a51
Psalmosky <oluwaseyiomotayo1@gmail.com>
2023-07-04 11:58:08 -0700

    Project's description

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number' || Number.isNaN(sqft) || sqft <= 0) {
-      throw new TypeError('Sqft must be a number');
-    }
+    if (!this.evacuationWarningMessage && this.constructor !== Building) throw Error('Class extending Building must override evacuationWarningMessage');
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // Abstract method
-
-  evacuationWarningMessage() {
-    if (this.constructor === Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
